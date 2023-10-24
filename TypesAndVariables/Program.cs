6e8b6efe48cd0dfb7cb35228af05cda9826f9b62
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 01:35:01 +0300

    C# Dersleri 14: Var Keyword İle Çalışmak

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -20,11 +20,14 @@ namespace TypesAndVariables
             short number3 = 32767; //+ - 32768 e kadar çıkabilen ve tam sayıları tutabilen bir veri tipidir.
             int number1 = 2147483647; //-2147483648 ile 2147483647 arasındaki sayılardan oluşabilir.
             long number2 = 9223372036854775807; //–9223372036854775808 ile 9223372036854775807 arasında değer depolayabilmektedir.
+            var number7 = 10;
+            number7 = 'A';
             Console.WriteLine("Number1 is {0}", number1);
             Console.WriteLine("Number2 is {0}", number2);
             Console.WriteLine("Number3 is {0}", number3);
             Console.WriteLine("Number4 is {0}", number4);
             Console.WriteLine("Number5 is {0}", number5);
+            Console.WriteLine("Number7 is {0}", number7);
             Console.WriteLine(Days.Friday); //output:Friday
             Console.WriteLine((int)Days.Friday); //output:9 -index değeri bir önceki elemanın değerine göre değişiklik gösteriyor.
             Console.WriteLine("Character is: {0}", (int)character);
