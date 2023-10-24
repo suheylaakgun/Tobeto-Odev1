5cac4bf7c2dbc2d398575ab45e39e106d5778fec
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 02:12:17 +0300

    C# Dersleri 18: Switch Bloğuyla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,18 +10,30 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            var number = 11;
-            if (number == 10)
+            var number = 20;
+            //if (number == 10)
+            //{
+            //    Console.WriteLine("Number is 10");
+            //}
+            //else if (number == 20) 
+            //{
+            //    Console.WriteLine("Number is 20");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number is not 10 or 20");
+            //}
+            switch (number)
             {
-                Console.WriteLine("Number is 10");
-            }
-            else if (number == 20) 
-            {
-                Console.WriteLine("Number is 20");
-            }
-            else
-            {
-                Console.WriteLine("Number is not 10 or 20");
+                case 10:
+                    Console.WriteLine("Number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Number is 20");
+                    break;
+                default:
+                    Console.WriteLine("Number is not 10 or 20");
+                    break;
             }
             Console.ReadLine();
         }
