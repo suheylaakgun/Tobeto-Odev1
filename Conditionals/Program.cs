083f5bfbb076843a8f07186879b35d8899b5e90f
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 01:45:02 +0300

    C# Dersleri 15: If Bloklarıyla Çalışmaya Başlamak

---
+++ Conditionals/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 11;
+            if (number == 10)
+            {
+                Console.WriteLine("Number is 10");
+            }
+            else
+            {
+                Console.WriteLine("Number is not 10");
+            }
+            Console.ReadLine();
+        }
+    }
+}
