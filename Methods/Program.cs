1cc49324baf1d8d76093cc8cbb8bde5bd8b46209
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 02:35:33 +0300

    C# Dersleri 21: Neden Metot Kullanıyoruz?

---
+++ Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Add();
+            Add();
+            Add();
+            Console.ReadLine();
+        }
+        static void Add()
+        {
+            Console.WriteLine("Added!!!!");
+        }
+    }
+}
