ab6414d3417a146d2212c0536baa97f6a6b19156
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 02:04:46 +0300

    C# Dersleri 17: Else If Bloğuyla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,8 +10,19 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            var number = 10;
-            Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
+            var number = 11;
+            if (number == 10)
+            {
+                Console.WriteLine("Number is 10");
+            }
+            else if (number == 20) 
+            {
+                Console.WriteLine("Number is 20");
+            }
+            else
+            {
+                Console.WriteLine("Number is not 10 or 20");
+            }
             Console.ReadLine();
         }
     }
