c8f8e77eb6fc9fad6858af67aeba74cff840a878
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 00:46:36 +0300

    C# Dersleri 6: Long Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -13,7 +13,9 @@ namespace TypesAndVariables
             //ValueTypes
             //Console.WriteLine("Hello World");
             int number1 = 2147483647; //-2147483648 ile 2147483647 arasındaki sayılardan oluşabilir.
+            long number2 = 9223372036854775807; //–9223372036854775808 ile 9223372036854775807 arasında değer depolayabilmektedir.
             Console.WriteLine("Number1 is {0}", number1);
+            Console.WriteLine("Number2 is {0}", number2);
             Console.ReadLine(); 
         }
     }
