b1868a55efbe9e0a4a0ef17322a53908eca305c2
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 00:57:50 +0300

    C# Dersleri 8: Byte Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -12,12 +12,14 @@ namespace TypesAndVariables
         {
             //ValueTypes
             //Console.WriteLine("Hello World");
+            byte number4 = 255; //0 ile 255 arasında değişen işaretsiz 8 bit (1 bayt) tamsayıları tutar.
             short number3 = 32767; //+ - 32768 e kadar çıkabilen ve tam sayıları tutabilen bir veri tipidir.
             int number1 = 2147483647; //-2147483648 ile 2147483647 arasındaki sayılardan oluşabilir.
             long number2 = 9223372036854775807; //–9223372036854775808 ile 9223372036854775807 arasında değer depolayabilmektedir.
             Console.WriteLine("Number1 is {0}", number1);
             Console.WriteLine("Number2 is {0}", number2);
             Console.WriteLine("Number3 is {0}", number3);
+            Console.WriteLine("Number4 is {0}", number4);
             Console.ReadLine(); 
         }
     }
