b89c2b9cc02eba6f374194e89921aad4b17e32a2
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 01:48:16 +0300

    C# Dersleri 16: Single Line If İle Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,15 +10,8 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            var number = 11;
-            if (number == 10)
-            {
-                Console.WriteLine("Number is 10");
-            }
-            else
-            {
-                Console.WriteLine("Number is not 10");
-            }
+            var number = 10;
+            Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
             Console.ReadLine();
         }
     }
