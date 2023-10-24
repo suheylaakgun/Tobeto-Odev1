9393eeed3349ebdbdbdacbc0e3483d5fb879eb18
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 02:20:42 +0300

    C# Dersleri 19: Demo: Çoklu Şartlarla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,7 +10,7 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            var number = 20;
+            var number = 200;
             //if (number == 10)
             //{
             //    Console.WriteLine("Number is 10");
@@ -23,17 +23,29 @@ namespace Conditionals
             //{
             //    Console.WriteLine("Number is not 10 or 20");
             //}
-            switch (number)
+            //switch (number)
+            //{
+            //    case 10:
+            //        Console.WriteLine("Number is 10");
+            //        break;
+            //    case 20:
+            //        Console.WriteLine("Number is 20");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Number is not 10 or 20");
+            //        break;
+            //}
+            if (number >= 0 && number <= 100)
             {
-                case 10:
-                    Console.WriteLine("Number is 10");
-                    break;
-                case 20:
-                    Console.WriteLine("Number is 20");
-                    break;
-                default:
-                    Console.WriteLine("Number is not 10 or 20");
-                    break;
+                Console.WriteLine("Number is between 0-100");
+            }
+            else if (number > 100 && number <= 200)
+            {
+                Console.WriteLine("Number is between 101-200");
+            }
+            else if (number > 200 || number < 0)
+            {
+                Console.WriteLine("Number is less than 0 or greater than 200");
             }
             Console.ReadLine();
         }
