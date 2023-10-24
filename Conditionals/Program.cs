8bc9eb95de66cbc32f2c8511b82ce1c23c99b7e8
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 02:25:56 +0300

    C# Dersleri 20: İç İçe If Bloklarıyla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,7 +10,7 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            var number = 200;
+            var number = 94;
             //if (number == 10)
             //{
             //    Console.WriteLine("Number is 10");
@@ -35,17 +35,24 @@ namespace Conditionals
             //        Console.WriteLine("Number is not 10 or 20");
             //        break;
             //}
-            if (number >= 0 && number <= 100)
+            //if (number >= 0 && number <= 100)
+            //{
+            //    Console.WriteLine("Number is between 0-100");
+            //}
+            //else if (number > 100 && number <= 200)
+            //{
+            //    Console.WriteLine("Number is between 101-200");
+            //}
+            //else if (number > 200 || number < 0)
+            //{
+            //    Console.WriteLine("Number is less than 0 or greater than 200");
+            //}
+            if (number < 100)
             {
-                Console.WriteLine("Number is between 0-100");
-            }
-            else if (number > 100 && number <= 200)
-            {
-                Console.WriteLine("Number is between 101-200");
-            }
-            else if (number > 200 || number < 0)
-            {
-                Console.WriteLine("Number is less than 0 or greater than 200");
+                if (number >= 90 && number < 95)
+                {
+                    Console.WriteLine("Number is less than 95(also 100) and greater equals than 90");
+                }
             }
             Console.ReadLine();
         }
