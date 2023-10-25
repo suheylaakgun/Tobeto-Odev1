fdc8d5d6d0a77846122b76b876138c6ebecfcf22
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 05:52:48 +0300

    C# Dersleri 37: Classlara Giriş

---
+++ Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Console.ReadLine();
+        }
+    }
+}
