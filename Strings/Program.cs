5e86dd46f936ba0333374fd6b2f7520e580590df
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 05:21:48 +0300

    C# Dersleri 35: String İle Çalışmaya Başlamak

---
+++ Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "Ankara";
+            //Console.WriteLine(city[0]);
+            foreach (var item in city) 
+            {
+                Console.WriteLine(item);
+            }
+            string city2 = "İstanbul";
+            string result = city + city2;
+
+            Console.WriteLine(String.Format("{0} {1}", city, city2));
+            //Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
