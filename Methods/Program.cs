9445f297d205f9203e6ea0f55c903bda02a13739
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 03:02:25 +0300

    C# Dersleri 24: Challenge : Ref Keyword İle Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,11 +10,17 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            Add();
-            Add();
-            Add();
-            var result = Add2(10);
-            Console.WriteLine(result);
+            //Add();
+            //Add();
+            //Add();
+            //var result = Add2();
+
+            int number1 = 20;
+            int number2 = 100;
+            var result2 = Add3(ref number1, number2);
+
+            Console.WriteLine(result2);
+            Console.WriteLine(number1);
             Console.ReadLine();
         }
         static void Add()
@@ -25,6 +31,12 @@ namespace Methods
         //Parametreye değer atanmışsa def. değer yerine atanan değer geçerli olur.
         static int Add2(int number1=20, int number2=30)
         {
+            var result = number1 + number2;
+            return result;
+        }
+        static int Add3(ref int number1, int number2)
+        {
+            number1 = 30;
             var result = number1 + number2;
             return result;
         }
