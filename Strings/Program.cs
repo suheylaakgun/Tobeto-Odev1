df6621d4f4088c4d2510e24adf381ac59a216807
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 05:39:58 +0300

    C# Dersleri 36: String Metodlarıyla Çalışmak

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -10,9 +10,34 @@ namespace Strings
     {
         static void Main(string[] args)
         {
+            //Intro();
+            string sentence = "My name is Engin Demiroğ";
+            var result = sentence.Length;
+            var result2 = sentence.Clone();
+            sentence = "My name is Derin Demiroğ";
+
+            bool result3 = sentence.EndsWith("ğ");
+            bool result4 = sentence.StartsWith("My name");
+
+            var result5 = sentence.IndexOf("namee"); //bulamazsa -1 döndürür
+            var result6 = sentence.IndexOf(" ");
+            var result7 = sentence.LastIndexOf(" ");
+            var result8 = sentence.Insert(0, "Hello, ");
+            var result9 = sentence.Substring(3,4);
+            var result10 = sentence.ToLower();
+            var result11 = sentence.ToUpper();
+            var result12 = sentence.Replace(" ", "-");
+            var result13 = sentence.Remove(2,5);
+
+            Console.WriteLine(result13);
+            Console.ReadLine();
+        }
+
+        private static void Intro()
+        {
             string city = "Ankara";
             //Console.WriteLine(city[0]);
-            foreach (var item in city) 
+            foreach (var item in city)
             {
                 Console.WriteLine(item);
             }
@@ -21,7 +46,6 @@ namespace Strings
 
             Console.WriteLine(String.Format("{0} {1}", city, city2));
             //Console.WriteLine(result);
-            Console.ReadLine();
         }
     }
 }
