cc28bd5fcc6ddd8b6df8106d7ef0a22ed17a5734
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 03:44:43 +0300

    C# Dersleri 28: Diziler

---
+++ Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3] { "Engin", "Derin", "Salih"};
+            students[0] = "Engin";
+            students[1] = "Derin";
+            students[2] = "Salih";
+
+            string[] students2 = { "Engin", "Derin", "Salih" };
+
+            foreach (var student in students2) 
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
