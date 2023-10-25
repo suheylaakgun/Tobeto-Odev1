97b347f5d858c315f5e7954d01da5e54e0f74e6c
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 03:32:20 +0300

    C# Dersleri 27: Params Keyword İle Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -22,8 +22,10 @@ namespace Methods
             //Console.WriteLine(result2);
             //Console.WriteLine(number1);
 
-            Console.WriteLine(Multiply(2,4));
-            Console.WriteLine(Multiply(2, 4, 5));
+            //Console.WriteLine(Multiply(2,4));
+            //Console.WriteLine(Multiply(2, 4, 5));
+
+            Console.WriteLine(Add4(2,3,4,5,6));
             Console.ReadLine();
         }
         static void Add()
@@ -52,6 +54,12 @@ namespace Methods
         static int Multiply(int number1, int number2, int number3)
         {
             return number1 * number2 * number3;
+        }
+
+        //Kural!: Params keywordü son parametreye yerleşmelidir.
+        static int Add4(int number, params int[] numbers)
+        {
+            return numbers.Sum();
         }
     }
 }
