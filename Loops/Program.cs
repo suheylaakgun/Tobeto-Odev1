22093519dea98fdd50b2193bf41e1b168464483d
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-25 04:52:35 +0300

    C# Dersleri 32: Do While Döngüsü

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -11,14 +11,26 @@ namespace Loops
         static void Main(string[] args)
         {
             //ForLoop();
+            //WhileLoop();
+            int number = 10;
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+
+            } while (number>=0);
+            Console.ReadLine();
+        }
+
+        private static void WhileLoop()
+        {
             int number = 100;
-            while (number >=0) 
+            while (number >= 0)
             {
                 Console.WriteLine(number);
                 number--;
             }
             Console.WriteLine("Now number is {0}", number);
-            Console.ReadLine();
         }
 
         private static void ForLoop()
