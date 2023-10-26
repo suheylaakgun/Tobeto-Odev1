7a3fe83b484dffeef4a53b1f59b1ff9ddbe26090
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-26 13:38:10 +0300

    C# Dersleri 39: Encapsulation

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -8,9 +8,24 @@ namespace Classes
 {
     class Customer
     {
+        //Field
+        //public string FirstName; 
+        
+        //Property
         public int Id { get; set; }
-        //public string FirstName { get; set; }
-        public string FirstName; //Bu tanım da çalışır ama kullanmama sebebi için encapsuliton dersine git!
+
+        //Bu örnek single responsibilitye aykırı bir durum.
+        private string _firstName; //Encapsuliton: Güvenlik için, nesne kullanıcısından get ve set işlemlerinin soyutlanması için kullanılan özelliktir.
+        public string FirstName { 
+            get 
+            {
+                return "Mrs." + _firstName; //Ör: Kullanıcı adı önüne oto takma ad koysun
+            } 
+            set 
+            { 
+                _firstName = value;
+            } 
+        }
         public string LastName { get; set; }
         public string City { get; set; }
     }
