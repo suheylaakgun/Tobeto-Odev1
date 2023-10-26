508de3a4595b6208da4009ae6eb6cf36021d167d
suheylaakgun <a.suheyladogan@gmail.com>
2023-10-27 02:05:10 +0300

    Pair çalışması : Lottery

--- PairPracticeLottery/Program.cs
+++ PairPracticeLottery/Program.cs
@@ -10,63 +10,77 @@ namespace PairPracticeLottery
     {
         static void Main(string[] args)
         {
+            //PracticeLotterySuh();
+            Console.WriteLine("Kolon sayısı giriniz:");
+            int columnCount = Convert.ToInt32(Console.ReadLine());
+            printArray(columnCount);
+        }
+
+        static void printArray(int columnCount)
+        {
+            if (columnCount >= 1 && columnCount <= 8)
+            {
+                Random rnd = new Random();
+                int[] numbers = new int[6];
+                for (int i = 1; i <= columnCount; i++)
+                {
+                    for (int j = 0; j < numbers.Length; j++)
+                    {
+                        int tmp = rnd.Next(1, 50);
+                        while (numbers.Contains(tmp)) //numbers dizisinde tmp sayısı varsa tekrar tmp üret.
+                        {
+                            tmp = rnd.Next(1, 50);
+                        }
+                        numbers[j] = tmp;
+                    }
+                    Console.WriteLine(i + ". kolon değerleri: " + string.Join(",", numbers));
+                    Console.ReadLine();
+                    /*for (int j = 0; j < numbers.Length; j++)
+                    {
+                        Console.Write(numbers[j] + " ");
+                    }
+                    Console.WriteLine();*/
+                }
+            }
+            else
+            {
+                Console.WriteLine("Kolon sayısı 1 ile 8 arasında olmalıdır!");
+            }
+        }
+
+        private static void PracticeLotterySuh()
+        {
             Console.WriteLine("Column enter:");
             int columnCount = Convert.ToInt32(Console.ReadLine());
-            if (columnCount >=1 && columnCount <=8)
+            if (columnCount >= 1 && columnCount <= 8)
             {
-                
+
                 Random rnd = new Random();
-                int tmp;
+
 
                 for (int i = 1; i <= columnCount; i++)
                 {
                     int[] arr = new int[6];
                     for (int j = 0; j < arr.Length; j++)
                     {
-                        tmp = rnd.Next(1, 49);
-                        if (IsDup(tmp, arr))
+                        int tmp = rnd.Next(1, 50);
+                        while (arr.Contains(tmp)) //numbers dizisinde tmp sayısı varsa tekrar tmp üret.
                         {
-                            //tmp = rnd.Next(1, 49);
-                            arr[j] = tmp;
+                            tmp = rnd.Next(1, 50);
                         }
-                        else
-                        {
-                            j--;
-                        }
+                        arr[j] = tmp;
                     }
-                    Console.WriteLine(i + "kolon değerleri: " + string.Join(",",arr));
+                    Console.WriteLine(i + "kolon değerleri: " + string.Join(",", arr));
                 }
                 //PrintTheArray(arr);
                 Console.ReadLine();
             }
-            else 
+            else
             {
                 Console.WriteLine("Hatalı giriş yaptınız!");
                 Console.WriteLine("Column enter:");
                 Console.ReadLine();
             }
-        }
-        //private static void PrintTheArray(int[] arr)
-        //{
-        //    foreach (var item in arr)
-        //    {
-        //        Console.WriteLine(item);
-        //    }
-        //}
-        private static bool IsDup(int tmp, int[] arr)
-        {
-            if (arr.Length == 0) 
-            {
-            }
-
-            foreach (var item in arr)
-            {
-                if (item == tmp)
-                {
-                    return true;
-                }
-            }
-            return false;
         }
 
     }
